50580ac99461f396ada168ad8e184290be02b0be
root <root@ip-10-0-0-7.ec2.internal>
2021-02-04 22:35:37 +0000

    Datbase Connected

--- trucker_app/settings.py
+++ trucker_app/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'trucker_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'my_database',
+        'HOST' : '10.0.0.26',
+        'PORT' : '27017',
     }
 }
 
